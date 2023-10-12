185ab5da3776b9764a04138215e61b008f4c41b9
tanaka <nitnc.tanaka.shikkobu@nezumi.be>
2023-10-12 11:44:41 +0900

    formatted

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "Software-Engineering-CUI",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "Software-Engineering-CUI"),
-    ]
+  name: "Software-Engineering-CUI",
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "Software-Engineering-CUI")
+  ]
 )
